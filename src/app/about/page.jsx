4b068ee9c870a40a467a0dca18e155ed89fe3a97
enemuo <francenemuo@gmail.com>
2024-05-28 14:56:54 +0100

    feat: add responsive navbar

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,36 @@
+"use client"
 import Link from 'next/link'
+import { Gauge, gaugeClasses } from '@mui/x-charts/Gauge';
+
+
 
 export default function About() {
   return (
     <div>
         <h2>About page</h2>
         <p><Link href='/'>Back to home</Link></p>
+
+          <h2>people are using us</h2>
+          <div style={{ width: 300, height: 500}}>
+  <Gauge value={68} 
+  cornerRadius="50%"
+   sx={(theme) => ({
+    [`& .${gaugeClasses.valueText}`]: {
+      fontSize: 70,
+    },
+    [`& .${gaugeClasses.valueArc}`]: {
+      fill: 'blue',
+    },
+    [`& .${gaugeClasses.referenceArc}`]: {
+      fill: '#bbb',
+    },
+  })}
+    />
+</div>
+
+
+
+            
     </div>
   )
 }
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,13 +1,35 @@
+"use client"
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
+import {FaCross, FaHamburger} from 'react-icons/fa'
 
 export default function Navbar() {
+    const [hide, setHide] = useState(false)
+
+    function showNav(){
+        setHide(!hide)
+    }
+
+    const score = 0
   return (
-    <nav className='bg-blue-700 text-white '>
+    <nav className='bg-blue-700 text-white flex sm:flex-row flex-col justify-between items-center'>
+        <div className='flex flex-row justify-between items-center sm:gap-0 gap-[400px]'>
+
         <ul className='flex flex-row justify-around items-center h-[10vh]'>
             <li>
                 <Link href='/'>Home</Link>
             </li>
+         </ul> 
+
+         <ul className='sm:hidden cursor-pointer'>
+            <li onClick={showNav}>
+                <FaHamburger className={hide ?'hidden': 'block'}/>
+                <FaCross className={!hide ?'hidden': 'block'}/>
+            </li>
+        </ul>
+        </div>
+
+         <ul className={hide ? 'absolute right-0 top-14 sm:top-0 bg-[#0066ffb7] sm:bg-transparent sm:flex sm:gap-5 sm:justify-end sm:items-center block h-[70vh] sm:h-[10vh] w-[80%] text-right px-12' :'sm:relative hidden sm:flex sm:flex-row gap-5 pr-5 sm:items-center  h-[10vh]'}>   
             <li>
                 <Link href='/about'>About</Link>
             </li>
@@ -17,7 +39,11 @@ export default function Navbar() {
             <li>
                 <Link href='/login'>Log In</Link>
             </li>
+            <li>
+                <span>{score}</span>
+            </li>
         </ul>
+       
     </nav>
   )
 }
