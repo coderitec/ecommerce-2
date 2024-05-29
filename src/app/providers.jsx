09871623d01daf98c2554abb4e0773a547e9eef9
enemuo <francenemuo@gmail.com>
2024-05-29 17:10:20 +0100

    fix: modify provider to be only on client

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -5,27 +5,32 @@ import React, { createContext, useEffect, useState } from 'react';
 const SidebarContext = createContext();
 
 const SidebarProvider = ({ children }) => {
-    const [cart, setCart] = useState(false)
-    const [cartItems, setCartItems] = useState(() => {
+    const [cart, setCart] = useState(false);
+    const [cartItems, setCartItems] = useState([]);
+    const [amount, setAmount] = useState(0);
+
+    useEffect(() => {
         const savedCartItems = localStorage.getItem('cartItems');
-        return savedCartItems !== null ? JSON.parse(savedCartItems) : [];
-    });
-    const [amount, setAmount] = useState(() => {
-        // Get the initial value from localStorage if available
         const savedAmount = localStorage.getItem('amount');
-        return savedAmount !== null ? parseInt(savedAmount, 10) : 0;
-    });
+
+        if (savedCartItems !== null) {
+            setCartItems(JSON.parse(savedCartItems));
+        }
+        if (savedAmount !== null) {
+            setAmount(parseInt(savedAmount, 10));
+        }
+    }, []);
+
     useEffect(() => {
         localStorage.setItem('cartItems', JSON.stringify(cartItems));
     }, [cartItems]);
 
     useEffect(() => {
-        // Save amount to localStorage whenever it changes
         localStorage.setItem('amount', amount);
     }, [amount]);
 
     return (
-        <SidebarContext.Provider value={{ amount, setAmount,cart,setCart, cartItems, setCartItems }}>
+        <SidebarContext.Provider value={{ amount, setAmount, cart, setCart, cartItems, setCartItems }}>
             {children}
         </SidebarContext.Provider>
     );
